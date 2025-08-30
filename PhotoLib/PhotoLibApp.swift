10d563ad5ee07db5411af6fa0ce231d83f96a4a0
Erie <erie@live.ru>
2025-08-30 13:04:03 +0900

    Initial Commit

---
+++ PhotoLib/PhotoLibApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoLibApp.swift
+//  PhotoLib
+//
+//  Created by Erie on 2025/08/30.
+//
+
+import SwiftUI
+
+@main
+struct PhotoLibApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PhotoLibTests/PhotoLibTests.swift
@@ -0,0 +1,16 @@
+//
+//  PhotoLibTests.swift
+//  PhotoLibTests
+//
+//  Created by Erie on 2025/08/30.
+//
+
+import Testing
+
+struct PhotoLibTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
